3bfb190a1e696e3b5bf893119c30cecb052b5831
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2022-10-07 14:08:03 +0200

    Add files via upload

--- desktop pet/pet.py
+++ desktop pet/pet.py
@@ -4,7 +4,14 @@ import tkinter as tk
 window = tk.Tk()
 import_path = "animations/"
 state = 0 #determines which behaviour
-pos_x = 100
+#idle state
+#sleep state
+#move state
+#wake up state
+start_pos_y = [800]
+pos_x = 1000
+task_bar_size = 40
+pos_y = 720 - task_bar_size - 100
 
 #[all animations]
 idle = [tk.PhotoImage(file=import_path+'idle.gif',format = 'gif -index %i' %(i)) for i in range(5)]#idle gif
@@ -13,11 +20,13 @@ sleep = [tk.PhotoImage(file=import_path+'sleep.gif',format = 'gif -index %i' %(i
 sleep_to_idle = [tk.PhotoImage(file=import_path+'sleep_to_idle.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep to idle gif
 walk_positive = [tk.PhotoImage(file=import_path+'walking_positive.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to left gif
 walk_negative = [tk.PhotoImage(file=import_path+'walking_negative.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to right gif
+kitty_sandwich = [tk.PhotoImage(file=import_path+'kitty_sandwich.gif',format = 'gif -index %i' %(i)) for i in range(15)]
 
 animations = {
     "idle":{
         "animation":idle,
-        "spd":100
+        "spd":200,
+        'repetition':20
         },
     "idle_to_sleep":{
         "animation":idle_to_sleep,
@@ -25,26 +34,30 @@ animations = {
     },
     "sleep":{
         "animation":sleep,
-        "spd":1000
+        "spd":800,
+        'repetition':5
     }
 }
 
+repetition = 0
 cycle = 0
 current_animation = "idle"
 animation_spd = animations.get(current_animation).get("spd")
 move_spd = 1
 frame = animations.get(current_animation).get("animation")[0] #current frame of the animation
 
-def update_geometry(x = 100,y = 540):
+def update_geometry(x = 500,y = 600):
     # print("update geometry")
     window.geometry('100x100+'+str(x)+"+"+str(y))
 
 def play_animation():
     global cycle
+    global repetition
    # print(f"cycle: {cycle}")
     cycle += 1
     if cycle >= len(animations.get(current_animation).get("animation")): 
         cycle = 0
+        repetition += 1
         
     frame = animations.get(current_animation).get("animation")[cycle]
     label.configure(image=frame)
@@ -52,11 +65,32 @@ def play_animation():
     window.after(animation_spd, update)
 
 def set_state(new_state):
-    pass
+    global state
+    global current_animation
+    global animation_spd
+    global cycle
+    global repetition
+    if new_state == state:
+        return
+    state = new_state
+    state_str = ""
+    match state:
+        case 0:
+            state_str = 'idle'
+        case 1:
+            state_str = 'sleep'
+        
+    current_animation = state_str
+    animation_spd = animations.get(current_animation).get('spd')
+    cycle = 0
+    repetition = 0
+    
+    print(f'enter state ',state_str)
     
 def determine_state():
     """picks new state every run"""
-    pass
+    new_state = random.randint(0,1)
+    set_state(new_state)
 
 def update():
     global pos_x
@@ -64,21 +98,21 @@ def update():
     
     match state:
         case 0:
-            pos_x+= move_spd
+            #pos_x+= move_spd
             if pos_x > 1920:
                 state = 1
         case 1:
-            pos_x-= move_spd
+            #pos_x-= move_spd
             if pos_x < 0:
                 state = 0
     
-    update_geometry(pos_x)
-    determine_state()
+    update_geometry(pos_x, pos_y)
+    if repetition >= animations.get(current_animation).get('repetition'): determine_state()
     window.after(1, play_animation)
     #window.after(1, update)
 
 def mouseClick(event):
-    print("mouse clicked")
+    print("meoew")
 
 #loop the program
 window.after(1,update)
