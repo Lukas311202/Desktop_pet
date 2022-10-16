bdd9e68a33255f2d5555ee03459d42157f9d76d7
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2022-10-16 10:45:21 +0200

    Update move_window.py

--- desktop pet/move_window.py
+++ desktop pet/move_window.py
@@ -1,13 +1,21 @@
 import pygetwindow as gw
 
 def main():
-    print(gw.getAllTitles())
+    #print(gw.getAllTitles())
     vs_window = gw.getWindowsWithTitle("Visual Studio Code")[0]
-    vs_window.moveTo(20,20)
+    #vs_window.moveTo(20,20)
+    print(vs_window._getWindowRect())
 
 def move_window(title, pos):
     window = gw.getWindowsWithTitle(title)[0]
     window.moveTo(pos[0],pos[1])
+
+def get_window_position(title) -> gw.Rect:
+    window = gw.getWindowsWithTitle(title)[0]
+    return window._getWindowRect()
+
+def window_exists(title):
+    return len(gw.getWindowsWithTitle(title)) > 0
 
 if __name__ == "__main__":
     main()
