471486388c562327a6e8c0b313fc48dbcc909efd
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2022-10-20 16:15:22 +0200

    Update white_cat.py

--- desktop pet/white_cat.py
+++ desktop pet/white_cat.py
@@ -13,10 +13,11 @@ def determine_state():
 def hiss(event):
     print('hiss') if random.randint(0,1) == 0 else print('meow')
 
+animation_path = 'animations/'
 cat = pet.pet()
 cat.pick_state = determine_state
-cat.MouseClickEvent = hiss
-idle_animation = [tk.PhotoImage(file="animations/idle.gif",format = 'gif -index %i' %(i)) for i in range(5)]
+# cat.MouseClickEvent = hiss
+idle_animation = pet.get_animation_from_path(animation_path+'idle.gif', 5)
 cat.add_state('idle', animation=idle_animation, behaviour= idle_state, animation_spd=pet.get_animation_spd_seconds(cat,170), repetition=10)
 cat.set_state('idle')
 cat.start()
